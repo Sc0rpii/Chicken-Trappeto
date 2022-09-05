e45ceb349a3b81aac634a5f80c628c2f065d3a5e
Sc0rpii <mirkofreschi66@gmail.com>
2022-09-05 10:36:43 +0200

    update 3.0

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ var n_img = 3;
 var corrente = 1;
 
 var circleImg = document.querySelectorAll(".circle");
-circleImg[corrente -1].style ="background-color: #d9d9d9; width: 20px; height: 20px";
+circleImg[corrente -1].style ="background-color: #2C344B; width: 20px; height: 20px";
+
+const menuLink = document.querySelectorAll(".menu");
+const checkbox = document.querySelector("#menu-btn");
+
+for(const link of menuLink){
+    link.addEventListener('click', function(){
+        checkbox.checked = false;
+    })
+}
 
 function succ() {
     corrente++
@@ -11,15 +20,13 @@ function succ() {
     }
     for(var i = n_img; i>0; i--){
         document.querySelector("#menu .menu-slide .img-slider img:nth-child(" + i +") ").style.display = "none";
-         circleImg[corrente -1].style ="background-color: #d9d9d9; width: 20px; height: 20px";
         
     }
     document.querySelector("#menu .menu-slide .img-slider img:nth-child(" + corrente + ") ").style.display = "block";
     
     for(var i = 0; i < n_img; i++){
         circleImg[i].style ="background-color: none; width: 15px; height: 15px";
-        circleImg[corrente -1].style ="background-color: #d9d9d9; width: 20px; height: 20px";
-        console.log(i);
+        circleImg[corrente -1].style ="background-color: #2C344B; width: 20px; height: 20px";
     }
     
 }
@@ -35,7 +42,6 @@ function prec(){
     document.querySelector("#menu .menu-slide .img-slider img:nth-child(" + corrente + ") ").style.display = "block";
     for(var i = 0; i < n_img; i++){
         circleImg[i].style ="background-color: none; width: 15px; height: 15px";
-        circleImg[corrente -1].style ="background-color: #d9d9d9; width: 20px; height: 20px";
-        console.log(i);
+        circleImg[corrente -1].style ="background-color: #2C344B; width: 20px; height: 20px";
     }
 }
